de3a79560f7df25963333a7c14cee44c8e93096d
Aryan Gulhane <aryangulhane11112020@gmail.com>
2025-05-31 02:02:24 +0530

    Update sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -4,7 +4,7 @@ import type { Image } from 'sanity'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-05-30'
+const apiVersion = '2023-05-03' // Using a stable API version
 
 export const client = createClient({
   projectId,
