8b0c00a45e8f13a1970c599f6435df3b4bae423c
Aryan Gulhane <aryangulhane11112020@gmail.com>
2025-05-31 09:36:32 +0530

    Cleanup: remove node_modules from git, update .gitignore, delete nested node_modules folders

--- studio/sanity/lib/live.ts
+++ studio/sanity/lib/live.ts
@@ -1,13 +1,17 @@
 // Querying with "sanityFetch" will keep content automatically updated
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
-import { defineLive } from "next-sanity";
+// Live content API is not compatible with current next-sanity and sanity client versions due to type conflicts.
+// As a workaround, export a basic fetch function using the existing client.
 import { client } from './client'
 
-export const { sanityFetch, SanityLive } = defineLive({ 
-  client: client.withConfig({ 
-    // Live content is currently only available on the experimental API
-    // https://www.sanity.io/docs/api-versioning
-    apiVersion: 'vX' 
-  }) 
-});
+/**
+ * Fetch data from Sanity using GROQ query.
+ * @param {string} query - GROQ query string
+ * @param {any[]} params - Query parameters
+ */
+export async function sanityFetch<T>(query: string, params: any[] = []): Promise<T> {
+  return client.fetch<T>(query, params)
+}
+
+// Note: Live content updates are disabled until next-sanity and @sanity/client are fully compatible.
