c3a5c531a9f2f6ac659520a1a078f35c2789bd75
Aryan Gulhane <aryangulhane11112020@gmail.com>
2025-05-31 12:26:30 +0530

    Update sanity.cli.ts

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,8 +1,12 @@
 import {defineCliConfig} from 'sanity/cli'
 
+// Use fallback values if environment variables are not set
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'krdza9oy'
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+
 export default defineCliConfig({
   api: {
-    projectId: 'krdza9oy',
-    dataset: 'production'
+    projectId,
+    dataset
   }
 }) 
